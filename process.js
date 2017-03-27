c9495674d434b23b8d87bfba0f5613b8ab478733
Christian Ceciliano <cristiancehi@gmail.com>
2017-03-27 07:27:25 -0600

    Dont miss a message

--- process.js
+++ process.js
@@ -5,13 +5,23 @@ let {readFile, getFrequencyTable} = require.main.require('./lib')
 let NUM = Number(process.env.NUM) || 4
 let RANK = Number(process.env.RANK) || 0
 
+let queue = []
+process.on('message', m => {
+  queue.push(m)
+})
+
 let waitMessage = (function * () {
   while (true) {
+    while (queue.length > 0) {
+      console.log(`${RANK} message fetched from queue`)
+      yield Promise.resolve(queue.shift())
+    }
     console.log(`${RANK} waiting message`)
     yield new Promise((resolve, reject) => {
       process.once('message', m => {
         console.log(`${RANK} message received`)
         resolve(m)
+        queue.shift()
       })
     })
   }
@@ -30,10 +40,16 @@ function resolveAfter2Seconds (x) {
 }
 
 (async function main () {
-  let message = waitMessage.next().value
   console.log(`Hello from ${RANK}-${NUM}`)
   await resolveAfter2Seconds()
   let to = (RANK + 1) % NUM
   sendMessage({to})
-  console.log(`${RANK}-${NUM}`, await message)
+  sendMessage({to})
+  console.log(`${RANK}-${NUM}`, await (waitMessage.next().value))
+  console.log(`${RANK}-${NUM}`, await (waitMessage.next().value))
+  exit()
 })().catch(console.error)
+
+function exit () {
+  process.removeAllListeners('message')
+}
